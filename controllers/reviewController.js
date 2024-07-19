ec6cc0fb47eb71f44b78bd93f9f3901d48f662f5
ndzamboni <ndzamboni@gmail.com>
2024-07-19 14:56:16 -0400

    adding reviews works and so does comments

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,13 +4,26 @@ const { getOrCreateRestroom } = require('../utils/getOrCreateRestroom');
 exports.createReview = async (req, res) => {
   try {
     const { placeId, rating, comment } = req.body;
-    const customerOnlyUse = rating.customer_only_use === 'on';
+    const userId = req.user.id;
+    const customerOnlyUse = req.body.rating.customer_only_use === 'on';
 
     const restroom = await getOrCreateRestroom(placeId);
 
+    // Check if the user has already left a review for this restroom
+    const existingReview = await Review.findOne({
+      where: {
+        restroom_id: restroom.id,
+        user_id: userId
+      }
+    });
+
+    if (existingReview) {
+      return res.status(400).json({ message: 'You have already reviewed this restroom.' });
+    }
+
     const newReview = await Review.create({
       restroom_id: restroom.id,
-      user_id: req.user.id,
+      user_id: userId,
       cleanliness: rating.cleanliness,
       accessibility: rating.accessibility,
       privacy_security: rating.privacy_security,
@@ -18,13 +31,21 @@ exports.createReview = async (req, res) => {
       customer_only_use: customerOnlyUse,
       content: comment,
     });
-
-    return res.status(201).json({ message: 'Review created successfully', review: newReview });
+    
+    res.render('reviews/list', { searchResult: restroom, reviews: [newReview], user: req.user, userHasReviewed: true });
   } catch (error) {
     console.error('Error creating review:', error);
     return res.status(500).json({ message: 'Server error' });
   }
+  
+
+  //   return res.status(201).json({ message: 'Review created successfully', review: newReview });
+  // } catch (error) {
+  //   console.error('Error creating review:', error);
+  //   return res.status(500).json({ message: 'Server error' });
+  // }
 };
+
 
 exports.getReviewsAndInfoById = async (req, res) => {
   try {
@@ -37,15 +58,21 @@ exports.getReviewsAndInfoById = async (req, res) => {
 
     const reviews = await Review.findAll({
       where: { restroom_id: id },
-      include: [{ model: Users, attributes: ['username'] }, { model: Comment, include: [Users] }]
+      include: [{ model: Users, attributes: ['username'] }]
     });
 
-    res.render('reviews/list', { searchResult: restroom, reviews, user: req.user });
+    let userHasReviewed = false;
+    if (req.user) {
+      userHasReviewed = reviews.some(review => review.user_id === req.user.id);
+    }
+
+    res.render('reviews/list', { searchResult: restroom, reviews, user: req.user, userHasReviewed });
   } catch (error) {
     console.error('Error fetching reviews:', error.message, error.stack);
     return res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
+
 
 exports.deleteReview = async (req, res) => {
   try {
