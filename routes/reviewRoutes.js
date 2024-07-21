d0a5443deee721be381c157f516358fe0c583e2a
ndzamboni <ndzamboni@gmail.com>
2024-07-20 20:14:14 -0400

    Comments work deleting comments works But takes you to a json page.
    adding reviews works and you can delete your review if there are other reviews.
    
    I think it's a routing issue.

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,13 +1,10 @@
-const { sequelize } = require('../config/connection');
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
 const isAuthenticated = require('../middleware/auth');
 
 router.post('/', isAuthenticated, reviewController.createReview);
-// router.post('/', reviewController.createReview);
-router.get('/:id', isAuthenticated, reviewController.getReviewsAndInfoById);
-// router.put('/:reviewId', reviewController.updateReview);
+router.get('/:id', reviewController.getReviewsAndInfoById);
 router.delete('/:reviewId', isAuthenticated, reviewController.deleteReview);
 
 module.exports = router;
